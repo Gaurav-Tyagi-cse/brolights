db37cf03ca0ab4f3ee94b9d7bd33ff4c7430d8ba
Gaurav Tyagi <118004334+Gaurav-Tyagi-cse@users.noreply.github.com>
2025-01-30 19:04:32 +0530

    text

--- script.js
+++ script.js
@@ -2,3 +2,38 @@
 function openModal(imageSrc) {
     document.getElementById('modalImage').src = imageSrc;
 }
+
+
+let images = [
+    './images/pre1.jpeg',
+    './images/img18.jpeg',
+    './images/pre13.jpeg',
+    './images/pre17.jpeg',
+    './images/pre11.jpeg',
+    './images/pre16.jpeg',
+    './images/img7.jpg',
+    './images/img11.jpg'
+];
+let currentIndex = 0;
+
+function openModal(index) {
+    currentIndex = index;
+    document.getElementById("modalImage").src = images[currentIndex];
+    document.getElementById("imageModal").classList.add("show");
+    document.getElementById("imageModal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("imageModal").classList.remove("show");
+    document.getElementById("imageModal").style.display = "none";
+}
+
+function changeImage(step) {
+    currentIndex += step;
+    if (currentIndex < 0) {
+        currentIndex = images.length - 1;
+    } else if (currentIndex >= images.length) {
+        currentIndex = 0;
+    }
+    document.getElementById("modalImage").src = images[currentIndex];
+}
